d2ffa891977bb74c5dd8a55bc3141289db14ce5e
MateuszADV <mateusz.adv@gmail.com>
2022-05-28 18:06:37 +0200

    Generowanie liczb losowych

---
+++ Rozdzial_2/4_Liczby_losowe.py
@@ -0,0 +1,12 @@
+# Rzut Kośćmi
+
+import random
+
+die1 = random.randint(1, 6)
+die2 = random.randrange(6) + 1
+
+total = die1 + die2
+
+print("Wyrzuciłeś: ", die1, " oraz ", die2, " i uzyskałeś sumę ", total)
+
+input("\n\nAby zakończyć wciśnij ENTER")
