86fe4cb17ff04472383e443ec60c3dedaa915c57
MateuszADV <mateusz.adv@gmail.com>
2022-06-08 17:44:14 +0200

    Słowniki

--- Udemy/Python Object And Data Structures/21_Lists_In_Python.py
+++ Udemy/Python Object And Data Structures/21_Lists_In_Python.py
@@ -3,3 +3,36 @@ my_list = ["String", 100, 23.56]
 
 print(my_list)
 print(len(my_list))
+
+my_list = ["one", "two", "three"]
+print(my_list[0])
+print(my_list[1:])
+another_list = ["four", "five"]
+new_list = my_list + another_list
+print("------DODAWANIE LIST-----------")
+print(my_list)
+print(another_list)
+print(new_list)
+new_list[0] = "ONE ALL CAPS"
+print(new_list)
+new_list.append("six")
+print(new_list)
+new_list.append("seven")
+print(new_list)
+
+print(new_list.pop())
+print(new_list)
+print(new_list.pop(0))
+print(new_list)
+
+print("-----------------SORTOWANIE-------------------------------")
+new_list = ['a', 'e', 'x', 'b', 'c']
+num_list = [4,1,9,3]
+new_list.sort()
+print(new_list)
+num_list.sort()
+print(num_list)
+num_list.reverse()
+print(num_list)
+num_list.pop(1)
+print(num_list)
---
+++ Udemy/Python Object And Data Structures/23_Slowniki.py
@@ -0,0 +1,11 @@
+my_dict = {"key1" : "value1", "key2" : "value2"}
+print(my_dict)
+print(my_dict["key1"])
+prices_lookup = {"apple": 2.99, "orange": 4.99, "milk": 5.20}
+print(prices_lookup)
+print(prices_lookup["apple"])
+
+d = {'k1':123, 'k2':[1,2,3], 'k3':{"insidekey":100}}
+print(d)
+print(d["k2"])
+print(d["k3"]["insidekey"])
